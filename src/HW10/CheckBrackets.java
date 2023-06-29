5c3f55932f271af3700b6cfbd804144420a4136b
SveKraus <kraus.svetlana@gmx.de>
2023-06-29 09:38:41 +0200

    Initial commit

--- src/HW10/CheckBrackets.java
+++ src/HW10/CheckBrackets.java
@@ -6,30 +6,31 @@ public class CheckBrackets {
     public static void main(String[] args) {
         String example1 = "(2 + 3) * (5 - 2)";
         String example2 = "(2 + 3 * (5 - 2)";
-        String example3 = "(2 + 3) * 5 - 2)";
-        String example4 = "((2 + 3) * 5 - 2)";
+        String example3 = "2 + 3) * (5 - 2)";
+
 
         System.out.println(example1 + "   " + checkBrackets(example1));
         System.out.println(example2 + "   " + checkBrackets(example2));
         System.out.println(example3 + "   " + checkBrackets(example3));
-        System.out.println(example4 + "   " + checkBrackets(example4));
+
     }
-    public static String checkBrackets(String example){
+
+    public static String checkBrackets(String example) {
         Stack<Character> stack = new Stack<>();
 
-        for (char ch: example.toCharArray()){
-            if (ch == '('){
+        for (char ch : example.toCharArray()) {
+            if (ch == '(') {
                 stack.push(ch);
             }
-            if (ch == ')'){
-                if (stack.isEmpty()){
+            if (ch == ')') {
+                if (stack.isEmpty()) {
                     return "скобка не открыта";
-                }else {
+                } else {
                     stack.pop();
                 }
             }
         }
-        if (!stack.isEmpty()){
+        if (!stack.isEmpty()) {
             return "скобка не закрыта";
         }
         return "все верно";
